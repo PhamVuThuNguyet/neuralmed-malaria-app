57ffeaab5ce9d497b933dd43a338bee48b861b3f
PhamVuThuNguyet <pvtnguyet.19it1@vku.udn.vn>
2023-03-31 21:20:05 +0700

    Add switch statement to get header by page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,23 @@
 import MainLayout from "./layouts/main-layout";
 import AllRecords from "./pages/AllRecords";
 import Dashboard from "./pages/Dashboard";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
+  const location = useLocation();
+  const header = () => {
+    switch (location.pathname) {
+      case "/allrecords":
+        return "All Records";
+      case "/dashboard":
+        return "Dashboard";
+      default:
+        return "";
+    }
+  };
   return (
-    <MainLayout header="Dashboard">
+    <MainLayout header={header()}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/allrecords" element={<AllRecords />} />
