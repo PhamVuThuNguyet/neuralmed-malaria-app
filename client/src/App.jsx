4d5a51b18071b01d8a2d03fee891c66b6c1ede85
PhamVuThuNguyet <pvtnguyet.19it1@vku.udn.vn>
2023-03-31 13:58:12 +0700

    Setup router for pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import MainLayout from "./layouts/main-layout";
 import AllRecords from "./pages/AllRecords";
 import Dashboard from "./pages/Dashboard";
-import Diagnosis from "./pages/Diagnosis";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <MainLayout header="Dashboard">
-      <Diagnosis />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/allrecords" element={<AllRecords />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </MainLayout>
   );
 }
--- client/src/components/SideBar/index.jsx
+++ client/src/components/SideBar/index.jsx
@@ -1,12 +1,15 @@
 import SidebarItem from "./sidebar-item";
 import styles from "../../styles/SideBar/sidebar.module.scss";
 import Logo from "./sidebar-logo";
+import { Link } from "react-router-dom";
 export default function SideBar(props) {
   return (
     <div className={styles.container}>
       <Logo />
       {props.pages.map((page) => (
-        <SidebarItem key={page.title} icon={page.icon} title={page.title} />
+        <Link to={page.to}>
+          <SidebarItem key={page.title} icon={page.icon} title={page.title} />
+        </Link>
       ))}
     </div>
   );
