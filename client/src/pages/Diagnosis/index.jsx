68e75f9918f6112dbe6738f16a0028afbb3ed82a
nguyen-chung-impact360 <chung.nguyen@impact360impact.com>
2023-03-31 22:47:55 +0700

    Init canvas

--- client/src/pages/Diagnosis/index.jsx
+++ client/src/pages/Diagnosis/index.jsx
@@ -1,6 +1,7 @@
 import styles from "../../styles/Diagnosis/diagnosis.module.scss";
 import { images } from "../../data/image-list";
 import ImageList from "../../components/ImageList";
+import EditingCanvas from "../../components/EditingCanvas";
 
 export default function Diagnosis() {
   return (
@@ -11,7 +12,14 @@ export default function Diagnosis() {
         }
       </div>
       <div className="col-span-5">
-        
+        <div className="grid grid-rows-5">
+          <div className="row-span-3">
+            <EditingCanvas/>
+          </div>
+          <div className="row-span-2">
+            
+          </div>
+        </div>
       </div>
     </div>
   );
