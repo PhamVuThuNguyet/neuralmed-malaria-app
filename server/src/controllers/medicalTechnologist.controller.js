9cc7b1f6e2819faea016316374ba070e50c9b9cf
htphong01 <thanhphonga4.hbt@gmail.com>
2023-04-01 23:02:20 +0700

    add function send sms, create swagger api docs

--- server/src/controllers/medicalTechnologist.controller.js
+++
@@ -1,5 +0,0 @@
-const medicalTechnologistService = require("../services/medicalTechnologist.service");
-
-class MedicalTechnologistController {}
-
-module.exports = new MedicalTechnologistController();
