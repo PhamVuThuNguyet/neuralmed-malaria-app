862fff9ba111ad30510cadbfaf1cf92aee05e7ae
nguyen-chung-impact360 <chung.nguyen@impact360impact.com>
2023-04-02 23:05:22 +0700

    fixing form and button css

--- client/src/pages/ImportData/index.jsx
+++ client/src/pages/ImportData/index.jsx
@@ -38,7 +38,7 @@ export default function ImportData() {
         <>
             <div className="w-full">
                 <Link to="/">
-                    <button className="px-10 bg-transparent text-blue-700 font-semibold  py-2 border border-gray-500 hover:border-blue-500 rounded">
+                    <button className={styles["back-button"]}>
                         <ArrowLeft />
                     </button>
                 </Link>
@@ -47,32 +47,28 @@ export default function ImportData() {
                 <div className={styles["form-cont"]}>
                     <form>
                         <div className="mb-6">
-                            <label htmlFor="fullname" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full name</label>
-                            <input type="text" name="fullname" id="fullname" className="shadow-sm bg-transparent border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-transparent dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+                            <label htmlFor="id">ID</label>
+                            <input type="text" name="id" id="id" required />
                         </div>
                         <div className="mb-6">
-                            <label htmlFor="id" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ID</label>
-                            <input type="text" name="id" id="id" className="shadow-sm bg-transparent border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-transparent dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+                            <label htmlFor="receive_dep">Receive Department</label>
+                            <input type="text" name="receive_dep" id="receive_dep" required />
                         </div>
                         <div className="mb-6">
-                            <label htmlFor="receive_dep" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Receive Department</label>
-                            <input type="text" name="receive_dep" id="receive_dep" className="shadow-sm bg-transparent border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-transparent dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+                            <label htmlFor="receive_doc">Receive Doctor ID</label>
+                            <input type="text" name="receive_doc" id="receive_doc" required />
                         </div>
                         <div className="mb-6">
-                            <label htmlFor="receive_doc" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Receive Doctor ID</label>
-                            <input type="text" name="receive_doc" id="receive_doc" className="shadow-sm bg-transparent border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-transparent dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+                            <label htmlFor="datetime">Datetime</label>
+                            <input type="date" name="datetime" id="datetime" required />
                         </div>
                         <div className="mb-6">
-                            <label htmlFor="datetime" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Datetime</label>
-                            <input type="date" name="datetime" id="datetime" className="shadow-sm bg-transparent border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-transparent dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+                            <label htmlFor="age">Age</label>
+                            <input type="number" name="age" id="age" required />
                         </div>
                         <div className="mb-6">
-                            <label htmlFor="age" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Age</label>
-                            <input type="number" name="age" id="age" className="shadow-sm bg-transparent border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-transparent dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
-                        </div>
-                        <div className="mb-6">
-                            <label htmlFor="address" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address</label>
-                            <input type="text" name="address" id="address" className="shadow-sm bg-transparent border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-transparent dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+                            <label htmlFor="address">Address</label>
+                            <input type="text" name="address" id="address" required />
                         </div>
                     </form>
                 </div>
@@ -115,12 +111,12 @@ export default function ImportData() {
             </div>
             <div className="w-full flex justify-end text-3xl gap-8">
                 <Link to="/">
-                    <button className="p-24 py-8 bg-transparent text-gray-700 font-semibold border border-gray-500 rounded">
+                    <button className={styles["cancel-button"]}>
                         CANCEL
                     </button>
                 </Link>
                 <Link to="/">
-                    <button className="p-24 py-8 bg-transparent text-white font-semibold bg-blue-500 rounded">
+                    <button className={styles["next-button"]}>
                         NEXT
                     </button>
                 </Link>
