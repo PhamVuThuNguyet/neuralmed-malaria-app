646f95fcfa0e18714670f22cf77ab007612fd15f
Thanhnguyen710 <nguyenvantothanh@gmail.com>
2023-04-06 22:30:33 +0700

    Create interceptor for checking accsessToken to get authorzied header

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,23 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api/v1'
+  baseURL: 'http://localhost:3001/api/v1',
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
+
+api.interceptors.request.use(
+  config => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
